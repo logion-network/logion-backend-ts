5f8039974d1ba3cd086ded6025fd4a1a698f8a86
Gérard Dethier <gerard@logion.network>
2022-03-17 09:18:59 +0100

    chore: simplify migration.

--- src/logion/migration/1647428322698-ChangeTransactionPrimaryKey.ts
+++ src/logion/migration/1647428322698-ChangeTransactionPrimaryKey.ts
@@ -6,18 +6,10 @@ export class ChangeTransactionPrimaryKey1647428322698 implements MigrationInterf
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "transaction" ADD "id" uuid NOT NULL DEFAULT gen_random_uuid()`);
         await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("block_number", "extrinsic_index", "id")`);
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("extrinsic_index", "id")`);
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
         await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("id")`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("extrinsic_index", "id")`);
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("block_number", "extrinsic_index", "id")`);
         await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "PK_transaction"`);
         await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "PK_transaction" PRIMARY KEY ("block_number", "extrinsic_index")`);
         await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "id"`);
