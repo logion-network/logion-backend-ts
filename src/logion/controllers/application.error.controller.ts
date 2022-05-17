41167af7b2323ba5e5edfb0884224a13be6a8377
Gérard Dethier <gerard@logion.network>
2022-05-17 12:00:26 +0200

    chore: log 500s.
    logion-network/logion-internal#473

--- src/logion/controllers/application.error.controller.ts
+++ src/logion/controllers/application.error.controller.ts
@@ -1,9 +1,15 @@
 import { injectable } from 'inversify';
 import { ErrorController } from 'dinoloop';
 
+import { Log } from "../util/Log";
+
+const { logger } = Log;
+
 @injectable()
 export class ApplicationErrorController extends ErrorController {
     internalServerError(): void {
+        logger.error(this.error.message);
+        logger.error(this.error.stack);
         this.response
             .status(500)
             .json({
