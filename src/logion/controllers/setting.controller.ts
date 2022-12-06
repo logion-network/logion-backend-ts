37d30954ea90ab60de03470443c28ab11133da9a
Gérard Dethier <gerard@logion.network>
2022-12-06 15:45:49 +0100

    feat: prepare API for guest LLO settings.
    logion-network/logion-internal#702

--- src/logion/controllers/setting.controller.ts
+++ src/logion/controllers/setting.controller.ts
@@ -1,8 +1,21 @@
 import { injectable } from 'inversify';
 import { ApiController, Controller, HttpPut, Async, HttpGet } from 'dinoloop';
 import { SettingRepository } from '../model/setting.model';
-import { AuthenticationService } from '@logion/rest-api-core';
+import { addTag, AuthenticationService, getDefaultResponsesWithAnyBody, setControllerTag, setPathParameters } from '@logion/rest-api-core';
+import { OpenAPIV3 } from "express-oas-generator";
 import { SettingService } from '../services/settings.service';
+
+export function fillInSpec(spec: OpenAPIV3.Document): void {
+    const tagName = 'Settings';
+    addTag(spec, {
+        name: tagName,
+        description: "Handling of Legal Officer Settings"
+    });
+    setControllerTag(spec, /^\/api\/setting.*/, tagName);
+
+    SettingController.fetchSettings(spec);
+    SettingController.createOrUpdate(spec);
+}
 
 @injectable()
 @Controller('/setting')
@@ -16,9 +29,19 @@ export class SettingController extends ApiController {
         super();
     }
 
+    static fetchSettings(spec: OpenAPIV3.Document) {
+        const operationObject = spec.paths["/api/setting/{legalOfficer}"].get!;
+        operationObject.summary = "Lists all Legal Officer's settings";
+        operationObject.description = "The authenticated user must be a LO attached to the node.";
+        operationObject.responses = getDefaultResponsesWithAnyBody();
+        setPathParameters(operationObject, {
+            legalOfficer: "The address of the LO"
+        });
+    }
+
     @Async()
-    @HttpGet('')
-    async fetchSettings(): Promise<{settings: Record<string, string>}> {
+    @HttpGet('/:legalOfficer')
+    async fetchSettings(_body: never, _legalOfficer: string): Promise<{settings: Record<string, string>}> {
         (await this.authenticationService.authenticatedUser(this.request))
             .require(user => user.isNodeOwner());
         const settings = await this.settingRepository.findAll();
@@ -29,9 +52,20 @@ export class SettingController extends ApiController {
         return { settings: responseBody };
     }
 
+    static createOrUpdate(spec: OpenAPIV3.Document) {
+        const operationObject = spec.paths["/api/setting/{legalOfficer}/{id}"].put!;
+        operationObject.summary = "Updates a Legal Officer's setting";
+        operationObject.description = "The authenticated user must be a LO attached to the node.";
+        operationObject.responses = getDefaultResponsesWithAnyBody();
+        setPathParameters(operationObject, {
+            legalOfficer: "The address of the LO",
+            id: "The key of the setting to update",
+        });
+    }
+
     @Async()
-    @HttpPut('/:id')
-    async createOrUpdate(body: { value: string }, id: string): Promise<void> {
+    @HttpPut('/:legalOfficer/:id')
+    async createOrUpdate(body: { value: string }, _legalOfficer: string, id: string): Promise<void> {
         (await this.authenticationService.authenticatedUser(this.request))
             .require(user => user.isNodeOwner());
         const value = body.value;
