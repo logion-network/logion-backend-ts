149db9b582d08ed7328986b1678fd7e1fe79c334
Benoit Devos <benoit@logion.network>
2023-04-03 15:34:23 +0200

    fix: failing migration for missing column verified_third_party.
    logion-network/logion-internal#846

--- src/logion/migration/1679997379864-AddStorageFees.ts
+++ src/logion/migration/1679997379864-AddStorageFees.ts
@@ -4,7 +4,7 @@ export class AddStorageFees1679997379864 implements MigrationInterface {
     name = 'AddStorageFees1679997379864'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "loc_request" DROP COLUMN "verified_third_party"`);
+        await queryRunner.query(`ALTER TABLE "loc_request" DROP COLUMN IF EXISTS "verified_third_party"`);
         await queryRunner.query(`ALTER TABLE "transaction" ADD "storage_fee" numeric(50)`);
         await queryRunner.query(`ALTER TABLE "transaction" RENAME "fee" TO "inclusion_fee" `);
     }
