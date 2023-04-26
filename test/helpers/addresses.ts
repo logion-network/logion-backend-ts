d8e7da9fc48f8cc3bc886e649106a59fb0ae8925
Benoit Devos <benoit@logion.network>
2023-04-26 09:47:25 +0200

    feat: validate sponsorship against legal officer and requester.
    logion-network/logion-internal#875

--- test/helpers/addresses.ts
+++ test/helpers/addresses.ts
@@ -6,3 +6,5 @@ export const BOB = "5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty";
 export const CHARLY = "5FLSigC9HGRKVhB9FiEo4Y3koPsNmBmLJbpXg2mp1hXcS59Y";
 
 export const ALICE_ACCOUNT = polkadotAccount(ALICE);
+export const BOB_ACCOUNT = polkadotAccount(BOB);
+export const CHARLY_ACCOUNT = polkadotAccount(CHARLY);
--- test/unit/services/sponsorship.service.spec.ts
+++ test/unit/services/sponsorship.service.spec.ts
@@ -3,9 +3,9 @@ import { Queries, UUID, Sponsorship } from "@logion/node-api";
 import { LocRequestRepository, FetchLocRequestsSpecification } from "../../../src/logion/model/locrequest.model.js";
 import { SponsorshipService } from "../../../src/logion/services/sponsorship.service.js";
 import { PolkadotService } from "@logion/rest-api-core";
-import { ALICE_ACCOUNT, BOB } from "../../helpers/addresses.js";
+import { ALICE_ACCOUNT, CHARLY_ACCOUNT, BOB_ACCOUNT } from "../../helpers/addresses.js";
 import { REQUESTER_ADDRESS } from "../controllers/locrequest.controller.shared.js";
-import { polkadotAccount } from "../../../src/logion/model/supportedaccountid.model.js";
+import { polkadotAccount, SupportedAccountId } from "../../../src/logion/model/supportedaccountid.model.js";
 
 const locRequestRepository: Mock<LocRequestRepository> = new Mock<LocRequestRepository>();
 const queries: Mock<Queries> = new Mock<Queries>();
@@ -22,9 +22,9 @@ describe("SponsorshipService", () => {
 
     const service = createService();
 
-    async function testError(sponsorshipId: UUID, expectedError: string) {
+    async function testError(sponsorshipId: UUID, expectedError: string, legalOfficer?: SupportedAccountId, requester?: SupportedAccountId) {
         expectAsyncToThrow(
-            () => service.validateSponsorship(sponsorshipId),
+            () => service.validateSponsorship(sponsorshipId, legalOfficer ? legalOfficer : ALICE_ACCOUNT, requester ? requester : REQUESTER_ADDRESS),
             expectedError
         )
     }
@@ -45,8 +45,18 @@ describe("SponsorshipService", () => {
         await testError(sponsorshipAlreadyOpenDifferentLLO, "This sponsorship is already used in an open/closed/voided LOC")
     )
 
+    it("throws error for wrong Legal Officer", async () => {
+        const wrongLegalOfficer = CHARLY_ACCOUNT;
+        await testError(validSponsorship, "This sponsorship is not applicable to your request", wrongLegalOfficer, REQUESTER_ADDRESS)
+    })
+
+    it("throws error for wrong Sponsored Account", async () => {
+        const wrongRequester = polkadotAccount("5GnPfHk6Y6qsqSYQ5V6rRSaWxHf86QW1b2DQDGft1FjDT2iN");
+        await testError(validSponsorship, "This sponsorship is not applicable to your request", ALICE_ACCOUNT, wrongRequester)
+    })
+
     it("validates a valid sponsorship", async () => {
-        await service.validateSponsorship(validSponsorship);
+        await service.validateSponsorship(validSponsorship, ALICE_ACCOUNT, REQUESTER_ADDRESS);
     })
 })
 
@@ -64,7 +74,7 @@ function mockQueries(sponsorshipId: UUID, sponsorship: Sponsorship | undefined) 
 function mockSponsorship(locId: UUID | undefined): Sponsorship {
     return {
         locId,
-        sponsor: polkadotAccount(BOB),
+        sponsor: BOB_ACCOUNT,
         legalOfficer: ALICE_ACCOUNT,
         sponsoredAccount: REQUESTER_ADDRESS
     }
